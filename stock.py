3537f425b930796b6b4a795e4f609985a175112e
ytx1991 <t.yu@chia.net>
2024-12-27 17:39:27 -0800

    Tolerant temporary failures

--- stock.py
+++ stock.py
@@ -29,14 +29,17 @@ for token_info in token_infos:
 @cached(clock)
 def is_market_open(logger) -> bool:
     url = "https://www.sharesdao.com:8443/util/market_status"
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        logger.error(f"Failed to get market status: {response.text}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.error(f"Failed to get market status: {response.text}")
+            return False
+    except Exception as e:
+        logger.error(f"Failed to get market status: {e}")
         return False
-
 
 def get_stock_id_by_symbol(symbol):
     global stock_ids
@@ -47,11 +50,11 @@ def get_stock_id_by_symbol(symbol):
 
 @cached(cache)
 def get_stock_price_from_dinari(symbol, logger):
-    stock_id = get_stock_id_by_symbol(symbol)
-    url = f"https://api.sbt.dinari.com/api/v1/stocks/price_summaries?stock_ids={stock_id}"
-    response = requests.get(url)
-    logger.debug(f"Fetching stock price for {symbol} {response.status_code}:{response.json()}")
     try:
+        stock_id = get_stock_id_by_symbol(symbol)
+        url = f"https://api.sbt.dinari.com/api/v1/stocks/price_summaries?stock_ids={stock_id}"
+        response = requests.get(url)
+        logger.debug(f"Fetching stock price for {symbol} {response.status_code}:{response.json()}")
         if response.status_code == 200:
             data = response.json()
             if data and len(data) > 0:
